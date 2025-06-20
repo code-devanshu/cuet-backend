fbabd8f5e1ac804358a575c762504f1c901b08de
code-devanshu <code.devanshu@gmail.com>
2025-06-21 00:04:34 +0530

    Fixed port issue

--- main.py
+++ main.py
@@ -69,4 +69,8 @@ async def upload_files(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
